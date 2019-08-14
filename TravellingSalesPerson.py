eeaee50dc5582296468ef9270e91981c79f32125
tomcarney <thomas.carney@griffithuni.edu.au>
2019-08-14 17:28:26 +1000

    Initial commit

---
+++ TravellingSalesPerson.py
@@ -0,0 +1,67 @@
+import math
+
+SIZE = 5
+MAX = 100
+
+class Node:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+def main():
+    totalD = 0  # HA
+    nodes = []
+    order = [i for i in range(280)]
+    fileName = "a280.tsp"
+    line = ""
+
+    while True:
+        # fileName = input("File name: ")
+        try:
+            file = open(fileName)
+            break
+        except FileNotFoundError:
+            print("Error: Failed to open file. Try again.")
+
+    while 'EOF' not in line:
+        line = file.readline()
+        if any(c.isalpha() for c in line):
+            pass
+        else:
+            currentLine = line.split()
+            nodes.append(Node(int(currentLine[1]), int(currentLine[2])))
+
+
+    best = currentTotal(nodes)
+    print(currentTotal(nodes))
+
+    for i in range(10):
+        nodes = scramble(nodes)
+        print(currentTotal(nodes))
+        if currentTotal(nodes) < best:
+            best = currentTotal(nodes)
+
+    print(order)
+    print("Best: ", best)
+
+
+
+def scramble(nodes):
+    
+    return nodes
+
+def currentTotal(nodes):
+    total = 0
+    for i in range(len(nodes) - 1):
+        currentDistance = calcDist(nodes[i], nodes[i+1])
+        total += currentDistance
+    return total
+
+
+def calcDist(curNode, nextNode):
+    return math.sqrt((curNode.x - nextNode.x) ** 2 + (curNode.y - nextNode.y) ** 2)
+
+
+if __name__ == "__main__":
+    main()
