8e8e1369d362e0cd035de87db8f7454c6732b8fc
tomcarney <thomas.carney@griffithuni.edu.au>
2019-08-24 10:48:14 +1000

    Starting Point

--- TravellingSalesPerson.py
+++ TravellingSalesPerson.py
@@ -3,7 +3,8 @@ import random
 
 SIZE = 5
 MAX = 100
-testSize = 100000
+testSize = 100
+POPSIZE = 300
 
 class Node:
     def __init__(self, x, y):
@@ -11,13 +12,94 @@ class Node:
         self.y = y
 
 
-def main():
-    totalD = 0  # haha
-    nodes = []
-    order = [i for i in range(52)]
-    fileName = "berlin52.tsp"
-    line = ""
+class Pop:
+    def __init__(self, fitness):
+        self.order = [i for i in range(52)]
+        self.fitness = fitness
 
+
+def main():
+    # Population of random orders created
+    population = [Pop(0) for i in range(POPSIZE)]
+
+    # file i/o handled.
+    fileName = "berlin52.tsp"
+    nodes = nodeInput(fileName)
+
+    for i in population:
+        i.order = shuffle(i.order)
+        i.fitness = 0
+
+    getBest(nodes, population)
+
+    #population = fixFitness(population)
+
+    for i in range(testSize):
+        fittestOrder = Pop(0)
+        for i in population:
+            if i.fitness > fittestOrder.fitness:
+                fittestOrder = i
+        population = makeNextGen(fittestOrder, nodes)
+
+    for i in population:
+        print(i.fitness)
+
+    print(getBest(nodes, population))
+
+
+def pickSeed(popultaion):
+    index = 0
+    r = random.randint(0, 1)
+
+    while(r > 0):
+        r = r - popultaion[index].fitness
+        index += 1
+    index -= 1
+    return popultaion[index]
+
+
+def fixFitness(population):
+    sum = 0
+    for i in range(len(population)):
+        sum += population[i].fitness
+
+    for i in range(len(population)):
+        population[i].fitness = population[i].fitness / sum
+    return population
+
+
+def getBest(nodes, population):
+    for i in range(POPSIZE):
+        current = calcDistance(nodes, population[i].order)
+        if i == 0:
+            best = current
+        if current < best:
+            best = current
+        population[i].fitness = 1/(current+1)
+    return best
+
+
+def makeNextGen(fittestLast, nodes):
+    nextGen = [Pop(0) for i in range(POPSIZE)]
+    for i in range(POPSIZE):
+        nextGen[i].order = fittestLast.order
+        fittestLast.setFitness(fittestLast.order, nodes)
+        nextGen[i].fitness = fittestLast.fitness
+        fittestLast.order = swap(fittestLast.order)
+    return nextGen
+
+def swap(order):
+    a = random.randint(0, (len(order) - 2))
+    b = a+1
+    temp = order[a]
+    order[a] = order[b]
+    order[b] = temp
+    return order
+
+
+def nodeInput(fileName):
+    nodes = []
+    line = ""
     while True:
         # fileName = input("File name: ")
         try:
@@ -33,18 +115,8 @@ def main():
         else:
             currentLine = line.split()
             nodes.append(Node(float(currentLine[1]), float(currentLine[2])))
+    return nodes
 
-    best = 10000000000
-    for i in range(testSize):
-        order = shuffle(order)
-        print(order)
-        current = calcDistance(nodes, order)
-        if current < best:
-            best = current
-        print(current)
-        i += 1
-
-    print(best)
 
 def shuffle(order):
     random.shuffle(order)
