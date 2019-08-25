6e3a70b667d289a67a98dc7bc830926057b5bc38
tomcarney <thomas.carney@griffithuni.edu.au>
2019-08-25 15:48:51 +1000

    Genetic Alg. done, project complete.

--- TravellingSalesPerson.py
+++ TravellingSalesPerson.py
@@ -1,11 +1,10 @@
 import math
 import random
+import time
+import sys
 
-SIZE = 42
-MAX = 100
-GENERATIONS = 5000
+#number of orders in each population
 POPSIZE = 200
-MUTATION = 1
 
 class Node:
     def __init__(self, x, y):
@@ -20,81 +19,46 @@ class Pop:
 
 
 def main():
+
+    fileName = sys.argv[1]
+    timer = int(sys.argv[2])
+    outFile = sys.argv[3]
+    nodes = nodeInput(fileName)
+
     # Population of random orders created
-    order = [i for i in range(SIZE)]
+    order = [i for i in range(len(nodes))]
     population = [Pop(0, order) for i in range(POPSIZE)]
 
-    # file i/o handled.
-    #fileName = "berlin52.tsp"
-    fileName = "myTsp12.tsp"
-    nodes = nodeInput(fileName)
-    GOAT = 1000000
+    GOAT = Pop(0, order)
 
     for i in population:
         i.order = shuffle(i.order)
         i.fitness = 0
 
-    population = setFitness(nodes, population)
+    population = setFitness(nodes, population) ##starting population
 
-    for i in range(GENERATIONS):
-        print("Generation", i + 1)
-        if getBest(nodes, population) < GOAT:
-            GOAT = getBest(nodes, population)
-        print("Best: ", GOAT)
+    start = time.time()
+    now = 0
+    while now < timer:
+        now = (time.time() - start)
+        current = getBest(population)
+        if current.fitness > GOAT.fitness:
+            GOAT.order = current.order.copy()
+            GOAT.fitness = current.fitness
         population = makeNextGen(population, nodes)
 
-    print(GOAT)
+    output(outFile, GOAT, nodes)
+    print(calcDistance(nodes, GOAT.order))
 
 
-def setFitness(nodes, population):
-    for i in population:
-        current = calcDistance(nodes, i.order)
-        i.fitness = (1 / (current + 1)) * 1000
-    population = fixFitness(population)
-    return population
-
-
-def fixFitness(population):
-    fitSum = 0
-    for i in population:
-        fitSum += i.fitness
-    for i in population:
-        i.fitness = i.fitness / fitSum
-    return population
-
-
-def displayPop(population):
-    for i in population:
-        print(i.fitness, i.order)
-
-
-def getBest(nodes, population):
+def getBest(population):
+    index = 0
     for i in range(POPSIZE):
-        current = calcDistance(nodes, population[i].order)
+        current = population[i].fitness
         if i == 0:
             best = current
-        if current < best:
+        if current > best:
             best = current
-        population[i].fitness = 1/(current+1)
-    return best
-
-
-def pickSeed(population):
-
-    index = 0
-    """
-    r = random.random()
-
-    while r > 0:
-        r = r - population[index].fitness
-        index += 1
-        if index >= len(population):
-            index = 1
-    index -= 1"""
-    fittest = 0
-    for i in range(POPSIZE):
-        if population[i].fitness > fittest:
-            fittest = population[i].fitness
             index = i
 
     return population[index]
@@ -108,21 +72,51 @@ def makeNextGen(population, nodes):
     return nextGen
 
 
+#the more fitness an object has, the more likely it will be picked in the following generation
+def pickSeed(population):
+    index = 0
+    num = random.random()
+
+    while num > 0:
+        num = num - population[index].fitness
+        index += 1
+        if index >= len(population):
+            index = 1
+    index -= 1
+
+    return population[index]
+
+
+def setFitness(nodes, population):
+    fitSum = 0
+
+    for i in population:
+        current = calcDistance(nodes, i.order)
+        i.fitness = 1 / current #makes it so that a smaller distance means higher fitness
+        fitSum += i.fitness
+
+    #normalises the fitness across all the objects
+    for i in population:
+        i.fitness = i.fitness / fitSum
+
+    return population
+
+
 def swap(order):
-    for i in range(MUTATION):
-        a = random.randint(0, (len(order) - 2))
-        b = a+1
-        temp = order[a]
-        order[a] = order[b]
-        order[b] = temp
+    a = random.randint(0, (len(order) - 2))
+    b = a+1
+    temp = order[a]
+    order[a] = order[b]
+    order[b] = temp
+
     return order
 
 
 def nodeInput(fileName):
     nodes = []
     line = ""
+
     while True:
-        # fileName = input("File name: ")
         try:
             file = open(fileName)
             break
@@ -136,6 +130,8 @@ def nodeInput(fileName):
         else:
             currentLine = line.split()
             nodes.append(Node(float(currentLine[1]), float(currentLine[2])))
+    file.close()
+
     return nodes
 
 
@@ -143,13 +139,31 @@ def shuffle(order):
     random.shuffle(order)
     return order
 
+
 def calcDistance(nodes, order):
     total = 0
-    for i in range(len(order) - 1):
-        currentDistance = math.sqrt((nodes[order[i]].x - nodes[order[i+1]].x) ** 2 + (nodes[order[i]].y - nodes[order[i+1]].y) ** 2)
+
+    for i in range(len(order)):
+        j = i + 1
+        if j >= len(order):
+            j = 0
+        currentDistance = math.hypot((nodes[order[i]].x - nodes[order[j]].x), (nodes[order[i]].y - nodes[order[j]].y))
         total += currentDistance
+
     return total
 
+
+def output(outFile, GOAT, nodes):
+
+    file = open(outFile, "w")
+    ans = round(calcDistance(nodes, GOAT.order), 2)
+    file.write("Shortest Length Found in time limit: ")
+    file.write(str(ans))
+    for i in GOAT.order:
+        file.write("\n")
+        file.write(str(i))
+
+    file.close()
 
 
 if __name__ == "__main__":
