5d7d0660b67131fef71753f91c917894105dca69
tomcarney <thomas.carney@griffithuni.edu.au>
2019-10-05 16:28:45 +1000

    Simple GUI I/O with MySQL db interaction.

--- GUI.py
+++ GUI.py
@@ -1,7 +1,7 @@
 from TravellingSalesPerson import *
 import numpy as np
+
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
 
 
 try:
@@ -10,67 +10,113 @@ except ImportError:
     print("The wxPython module is required to run this program")
 
 
-class myGUI(wx.Frame):
-    def __init__(self, parent, id, title):
-        wx.Frame.__init__(self, parent, id, title, size=(600, 500))
-        self.parent = parent
-        self.pnl = wx.Panel(self)
-        fileBox = wx.TextCtrl(self, style=wx.TE_PROCESS_ENTER, size=(75, 25))
+#########################################################################################
+# TO USE:                                                                               #
+# All file input is just the name, no .tsp, etc. (e.g. 'a280')                          #
+# Time is in seconds, and is the amount of time you would like to solve for             #
+# Upload data: Uploads local file to db                                                 #
+# Load data: Loads file from a db to be used locally                                    #
+# Show data: shows a plot of all the points in a local file                             #
+# Solve: solve a loaded file for the given time                                         #
+# Display solution: displays the local solve                                            #
+# Load solution: Loads a solution from the data base and displays it                    #
+# Save solution: saves the last solution to the db.                                     #
+#########################################################################################
 
-        fileName = wx.StaticText(self.pnl, label="File: ")
+
+GOAT = ""
+dist = 0
+
+class myGUI(wx.Frame):
+    def __init__(self, *args, **kw):
+        super(myGUI, self).__init__(*args, **kw, size=(900, 200))
+        self.init()
+
+    def init(self):
+        pnl = wx.Panel(self)
+
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        progBox = wx.BoxSizer(wx.HORIZONTAL)
+        buttonSizer = wx.BoxSizer(wx.HORIZONTAL)
+
+        TimerName = wx.StaticText(pnl, label="Time: ")
+        font = TimerName.GetFont()
+        font.PointSize += 3
+        TimerName.SetFont(font)
+
+        fileName = wx.StaticText(pnl, label="File: ")
         font = fileName.GetFont()
         font.PointSize += 3
         fileName.SetFont(font)
 
+        self.timerBox = wx.TextCtrl(pnl, style=wx.TE_PROCESS_ENTER, size=(100, 25))
+        self.fileBox = wx.TextCtrl(pnl, style=wx.TE_PROCESS_ENTER, size=(100, 25))
+        Upload = wx.Button(pnl, label="Upload Data")
+        disCurSol = wx.Button(pnl, label="Display Solution")
+        loadFrom = wx.Button(pnl, label="Load Data")
+        solve = wx.Button(pnl, label="Solve")
+        show = wx.Button(pnl, label="Show Data")
+        fetch = wx.Button(pnl, label="Load Solution")
+        save = wx.Button(pnl, label="Save Solution")
 
-
-        #self.fig = plt.figure()
-        #plt.plot([(2, 2), (3, 3)])
-        # self.axes = self.fig.add_subplot(111)
-       #self.canvas = FigureCanvas(self, -1, self.fig)
-        # self.sizer = wx.BoxSizer(wx.HORIZONTAL)
-        # self.sizer.Add(self.canvas, 1, wx.BOTTOM)
-        # self.SetSizerAndFit(self.sizer)
-
-        add = wx.Button(self, label="Add")
-        self.Bind(wx.EVT_BUTTON, self.onButton, add)
-        fetch = wx.Button(self, label="Fetch")
+        self.Bind(wx.EVT_BUTTON, self.onButton, Upload)
         self.Bind(wx.EVT_BUTTON, self.onButton, fetch)
-        solve = wx.Button(self, label="Solve")
+        self.Bind(wx.EVT_BUTTON, self.onButton, loadFrom)
         self.Bind(wx.EVT_BUTTON, self.onButton, solve)
-        show = wx.Button(self, label="Show")
         self.Bind(wx.EVT_BUTTON, self.onButton, show)
+        self.Bind(wx.EVT_BUTTON, self.onButton, save)
+        self.Bind(wx.EVT_BUTTON, self.onButton, disCurSol)
 
+        progBox.Add(TimerName, 0)
+        progBox.Add(self.timerBox, 0)
+        progBox.AddSpacer(10)
+        progBox.Add(fileName, 0)
+        progBox.Add(self.fileBox, 0, 10)
 
-        buttonSizer = wx.BoxSizer(wx.HORIZONTAL)
-        #buttonSizer = wx.GridSizer(4, 2, 0)
-        #buttonSizer.Add(self.canvas)
-
-        buttonSizer.Add(fileName, 0)
-        ###buttonSizer.Add(10, 0, 0)
-        buttonSizer.Add(fileBox, 0)
-        buttonSizer.Add(add, 0)
-        buttonSizer.Add(solve, 0)
-        buttonSizer.Add(fetch, 0)
+        buttonSizer.Add(Upload, 0)
+        buttonSizer.AddSpacer(10)
+        buttonSizer.Add(loadFrom, 0)
+        buttonSizer.AddSpacer(10)
         buttonSizer.Add(show, 0)
+        buttonSizer.AddSpacer(10)
+        buttonSizer.Add(solve, 0)
+        buttonSizer.AddSpacer(10)
+        buttonSizer.Add(disCurSol, 0)
+        buttonSizer.AddSpacer(10)
+        buttonSizer.Add(fetch, 0)
+        buttonSizer.AddSpacer(10)
+        buttonSizer.Add(save, 0)
 
-        self.SetSizer(buttonSizer)
-        #sizer.Add(buttonSizer, proportion=1)
+        vbox.Add((0, 30))
+        vbox.Add(progBox, proportion=1, flag=wx.ALIGN_CENTER)
+        vbox.Add((0, 30))
+        vbox.Add(buttonSizer, proportion=1, flag=wx.ALIGN_CENTER)
+        vbox.Add((0, 30))
 
-        #self.SetSizerAndFit(sizer)
+        pnl.SetSizer(vbox)
+        #self.Centre()
 
         self.Show(True)
 
     def onButton(self, event):
+        global GOAT
+        global dist
         fn = self.fileBox.GetValue()
         fnTSP = fn + ".tsp"
         button = event.GetEventObject()
-        if button.GetLabel() == "Add":
+        toBeSolved = main(fn, "LoadForSolve", None)
+
+        if button.GetLabel() == "Solve":
+            solveTime = int(self.timerBox.GetValue())
+            print("Solving...")
+            GOAT, dist = solve(toBeSolved, solveTime)
+            print("Solve Successful. ", dist)
+        if button.GetLabel() == "Upload Data":
             main(fn, "ADD", fnTSP)
             print("Successfully added.")
-        elif button.GetLabel() == "Fetch":
+        if button.GetLabel() == "Load Solution":
             nodes = main(fn, "SHOW", "")
-            nodesOrder = main(fn, "FETCH", "")
+            nodesOrder = main(fn, "FETCH", None)
             nodesOrder = nodesOrder[0].replace('[', '').replace(']', '').split(",")
             list1 = []
             list2 = []
@@ -82,15 +128,9 @@ class myGUI(wx.Frame):
             x = np.array(list1)
             y = np.array(list2)
             plt.plot(x, y)
-
             plt.show()
             print("Fetch Successful.")
-        elif button.GetLabel() == "Solve":
-            print("Solving...")
-            main(fn, "SOLVE", "120")
-            print("Solve Successful.")
-
-        elif button.GetLabel() == "Show":
+        if button.GetLabel() == "Show Data":
             print("Displaying data.")
             nodes = main(fn, "SHOW", "")
             list1 = []
@@ -100,13 +140,39 @@ class myGUI(wx.Frame):
                 list2.append(j)
             plt.plot(list1, list2, "ob")
             plt.show()
-
-class MyPanel(wx.Panel):
-    def __init__(self, parent):
-        super(MyPanel, self).__init__(parent)
+        if button.GetLabel() == "Load Data":
+            print("Loading from db.")
+            main(fn, "LoadForSolve", None)
+            print("Load successful.")
+        if button.GetLabel() == "Save Solution":
+            today = date.today()
+            solveTime = int(self.timerBox.GetValue())
+            print("Saving current solution.")
+            format_str = """INSERT INTO Solution(Tour, TourLength, ProblemName, Author, date, Algorithm, RunningTime) VALUES ("{s}", "{d}", "{n}", "{a}", "{day}", "{Alg}", "{time}");"""
+            sql_command = format_str.format(s=GOAT, d=dist, n=fn, a="5130828", day=today, Alg="Genetic", time=solveTime)
+            main(fn, "SaveNewSolve", sql_command)
+            print("Save successful.")
+        if button.GetLabel() == "Display Solution":
+            nodes = main(fn, "SHOW", "")
+            list1 = []
+            list2 = []
+            try:
+                for i in GOAT:
+                    i = int(i)
+                    x, y = nodes[i]
+                    list1.append(x)
+                    list2.append(y)
+                x = np.array(list1)
+                y = np.array(list2)
+                plt.plot(x, y)
+                plt.show()
+            except UnboundLocalError:
+                print("Please make a solve before trying to display local solved.")
+        if button.GetLabel() == "Save Solution":
+            main(fn, "SaveNewSolve", None)
 
 
 if __name__ == "__main__":
     app = wx.App()
-    frame = myGUI(None, -1, "TSP Solver")
+    frame = myGUI(None)
     app.MainLoop()
--- TravellingSalesPerson.py
+++ TravellingSalesPerson.py
@@ -1,119 +1,111 @@
-import sys
-import sqlite3
 from solver import *
+import mysql.connector
+from mysql.connector import Error
+from datetime import date
 
 
-DBNAME = "TSPdb.db"
+
+#DBNAME = 's5130828db'
+DBNAME = '1810ICTdb'
 
 
 def main(arg1, arg2, arg3):
-    connection = sqlite3.connect(DBNAME)
-    cursor = connection.cursor()
+    GOAT = ""
+    nodes = []
+    dist = 0
 
     try:
-        name = arg1
-        task = arg2
-    except IndexError:
-        print("Error: Missing command line argument(s). Try again")
-        exit()
+        connection = mysql.connector.connect(host='mysql.ict.griffith.edu.au',
+                                         user='s5130828',
+                                         password='aSPYbvce',
+                                            database=DBNAME)
+        if connection.is_connected():
+            cursor = connection.cursor()
+            cursor = connection.cursor(buffered=True)
+            try:
+                name = arg1
+                task = arg2
+            except IndexError:
+                print("Error: Missing command line argument(s). Try again")
+                exit()
 
-    try:
-        extra = arg3
-    except IndexError:
-        if task != "FETCH":
-            print("Error: Missing command line argument(s). Try again")
-            exit()
+            try:
+                extra = arg3
+            except IndexError:
+                if task != "FETCH":
+                    print("Error: Missing command line argument(s). Try again")
+                    exit()
 
 
-    try:
-        cursor.execute("select * from problem_sets;")
-    except sqlite3.OperationalError:
-        print("Error: Could not connect to database. Try again.")
-        exit()
+            try:
+                cursor.execute("select * from Problem;")
+            except Error:
+                print("Error: Could not connect to database. Try again.")
+                exit()
 
-    if task == "ADD":
-        nodes, size = nodeInput(extra)
-        #insert into problem_sets table
-        format_str = """INSERT INTO problem_sets(name, size) VALUES ("{curName}", "{curSize}");"""
-        sql_command = format_str.format(curName=name, curSize=size)
-        cursor.execute(sql_command)
-
-        #insert into data table
-        for i in nodes:
-            format_str = """INSERT INTO data(x, y, problem_name) VALUES ("{x}", "{y}", "{curName}");"""
-            sql_command = format_str.format(x=i.x, y=i.y, curName=name)
-            cursor.execute(sql_command)
-        connection.commit()
-    elif task == "SOLVE":
-        nodes = []
-        format_str = "select x,y from data WHERE problem_name = \"{n}\""
-        sql_command = format_str.format(n=name)
-        cursor.execute(sql_command)
-        result = cursor.fetchall()
-        if result == []:
-            print("Error: Tried to solve problem but no problem existed. Try again.")
-            exit()
-        for r in result:
-            for i in range(len(r) - 1):
-                nodes.append(Node(float(r[1]), float(r[i+1])))
-        extra = int(extra)
-        GOAT, dist = solve(nodes, extra)
-        GOAT = str(GOAT)
-
-        #check if we have already solved this, if we have, store the better solution.
-        format_str = """select distance from solutions where problem_name = \"{n}\""""
-        sql_command = format_str.format(n=name)
-        cursor.execute(sql_command)
-        result = cursor.fetchone()
-        strResult = str(result)
-        if strResult != "None":
-            for i in result:
-                result = i
-            if result < dist:
-                print("Previous solution was more efficient. Keeping previous solution")
-                return
-            else:
-                print("New solution is more efficient, Replacing the previous solution.")
-                format_str = """delete from solutions where problem_name = \"{n}\""""
-                sql_command = format_str.format(n=name)
+            if task == "ADD":
+                nodes, size = nodeInput(extra)
+                #insert into problem_sets table
+                format_str = """INSERT INTO Problem(name, size, comment) VALUES ("{curName}", "{curSize}", " ");"""
+                sql_command = format_str.format(curName=name, curSize=size)
                 cursor.execute(sql_command)
 
-        format_str = """INSERT INTO solutions(shortest_order, distance, problem_name) VALUES ("{s}", "{d}", "{n}");"""
-        sql_command = format_str.format(s=GOAT, d=dist, n=name)
-        cursor.execute(sql_command)
-        connection.commit()
-    elif task == "FETCH":
-        format_str = "select distance, shortest_order from solutions WHERE problem_name = \"{n}\""
-        sql_command = format_str.format(n=name)
-        cursor.execute(sql_command)
-        result = cursor.fetchone()
-        strResult = str(result)
-        if strResult == "None":
-            print("Error: Tried to fetch solution that does not exist. Try again.")
-            exit()
-        for i in range(len(result)):
-            returning = []
-            if i == 0:
-                pass
-                #print("Distance: ")
-                #print(result[i])
-            else:
-               # print("Order: ")
-                #print(result[i])
-                returning.append(result[i])
-        return returning
-    elif task == "SHOW":
-        format_str = "select x,y from data WHERE problem_name = \"{n}\""
-        sql_command = format_str.format(n=name)
-        cursor.execute(sql_command)
-        result = cursor.fetchall()
-        if result == []:
-            print("Error: Tried to solve problem but no problem existed. Try again.")
-            exit()
-        return result
+                #insert into data table
+                icounter = 0
+                for i in nodes:
+                    format_str = """INSERT INTO Cities(Name, ID, x, y) VALUES ( "{curName}", "{ID}", "{x}", "{y}");"""
+                    sql_command = format_str.format(x=i.x, y=i.y, curName=name, ID=icounter)
+                    cursor.execute(sql_command)
+                    icounter += 1
+                connection.commit()
+            elif task == "LoadForSolve":
+                format_str = "select x,y from Cities WHERE name = \"{n}\""
+                sql_command = format_str.format(n=name)
+                cursor.execute(sql_command)
+                result = cursor.fetchall()
+                if result == []:
+                    print("Error: Tried to solve problem but no problem existed. Try again.")
+                    exit()
+                for r in result:
+                    for i in range(len(r) - 1):
+                        nodes.append(Node(float(r[1]), float(r[i+1])))
+                return nodes
+            elif task == "SaveNewSolve":
+                cursor.execute(extra)
+                connection.commit()
+            elif task == "FETCH":
+                format_str = "SELECT Tour FROM Solution WHERE ProblemName = \"{n}\" AND Author = \"{a}\""
+                sql_command = format_str.format(n=name, a="5130828")
+                cursor.execute(sql_command)
+                result = cursor.fetchone()
+                strResult = str(result)
+                if strResult == "None":
+                    print("Error: Tried to fetch solution that does not exist. Try again.")
+                    exit()
+                curTour = []
+                for i in result:
+                    curTour.append(i)
+                return curTour
+            elif task == "SHOW":
+                format_str = "select x,y from Cities WHERE Name = \"{n}\""
+                sql_command = format_str.format(n=name)
+                cursor.execute(sql_command)
+                result = cursor.fetchall()
+                if result == []:
+                    print("Error: Tried to solve problem but no problem existed. Try again.")
+                    exit()
+                return result
 
-    connection.close()
+            connection.close()
 
+    except Error as e:
+        print("Error: Failed to connect to MySQL ", e)
+
+
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("MySQL connection is closed.")
 
 if __name__ == "__main__":
     main()
